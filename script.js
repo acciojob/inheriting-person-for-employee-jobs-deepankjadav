8548cc9271b616dba593886d9e5ecc102647e435
deepankjadav <jadavdeepank@gmail.com>
2024-11-26 16:31:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// complete this js code
 class Person {
     constructor(name, age) {
         this.name = name;
@@ -9,17 +10,18 @@ class Person {
     }
 }
 
+// Define the Employee class that inherits from Person
 class Employee extends Person {
     constructor(name, age, jobTitle) {
+        // Call the parent class's constructor
         super(name, age);
         this.jobTitle = jobTitle;
     }
 
     jobGreet() {
-        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
     }
 }
-
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
