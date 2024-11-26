2e3457e7ec6c92cda12a0bfe31e1eba99210e6e1
deepankjadav <jadavdeepank@gmail.com>
2024-11-26 16:32:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 // complete this js code
-class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    greet() {
-        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-    }
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
 }
 
-// Define the Employee class that inherits from Person
-class Employee extends Person {
-    constructor(name, age, jobTitle) {
-        // Call the parent class's constructor
-        super(name, age);
-        this.jobTitle = jobTitle;
-    }
+Person.prototype.greet = function () {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
 
-    jobGreet() {
-        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-    }
+function Employee(name, age, jobTitle) {
+    // Call the Person constructor to initialize name and age
+    Person.call(this, name, age);
+    this.jobTitle = jobTitle;
 }
+
+// Set up inheritance from Person
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function () {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
+
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
